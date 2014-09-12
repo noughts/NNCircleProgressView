7c7369263ea6ef9a0fff32ade9441e4d6d5fe34f
koichi yamamoto <koichi@dividual.jp>
2014-09-12 18:50:33 +0900

    a

--- NNCircleProgressView/NNCircleProgressView.m
+++ NNCircleProgressView/NNCircleProgressView.m
@@ -13,8 +13,6 @@
 	CGFloat _progress;
 	UIColor* _color;
 	CGFloat _intercept;// 円弧表示の切片
-	NSTimer* _timer;
-	BOOL _beat;
 }
 
 -(void)awakeFromNib{
@@ -27,13 +25,13 @@
 	
 	CGFloat rad1 = 0 * M_PI / 180;
 	CGFloat rad2 = 360 * M_PI / 180;
-    _arc.path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(0, 0) radius:radius startAngle:rad1 endAngle:rad2 clockwise:NO].CGPath;
+    _arc.path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(0, 0) radius:radius startAngle:rad1 endAngle:rad2 clockwise:YES].CGPath;
 	_arc.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
 	
     _arc.fillColor = [UIColor clearColor].CGColor;
     _arc.strokeColor = _color.CGColor;
     _arc.lineWidth = 2;
-	_arc.strokeEnd = 0;
+	_arc.strokeEnd = _intercept;
 	
     [self.layer addSublayer:_arc];
 }
@@ -52,44 +50,26 @@
     rotationAnimation.cumulative = YES;
     rotationAnimation.repeatCount = HUGE_VALF;
     [self.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
-	
-	_timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(time:) userInfo:nil repeats:YES];
-}
-
-
--(void)time:(NSTimer*)timer{
-	if( _beat ){
-		[self setArcSizeFrom:0.1 to:0.5];
-	} else {
-		[self setArcSizeFrom:0.5 to:0.1];
-	}
-	_beat = !_beat;
 }
 
 
 -(void)setProgress:(CGFloat)progress{
-	_progress = progress;
-}
-
--(void)setArcSizeFrom:(CGFloat)from to:(CGFloat)to{
 	CABasicAnimation *drawAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    drawAnimation.duration = 0.6;
-    drawAnimation.fromValue = @(from);
-    drawAnimation.toValue   = @(to);
+    drawAnimation.duration = 0.25;
+	// 進捗が0の時にも線が見えるようにする
+	CGFloat from = (1-_intercept) * _progress;
+	CGFloat to = (1-_intercept) * progress;
+    drawAnimation.fromValue = [NSNumber numberWithFloat:_intercept+from];
+    drawAnimation.toValue   = [NSNumber numberWithFloat:_intercept+to];
 	drawAnimation.removedOnCompletion = NO;
 	drawAnimation.fillMode = kCAFillModeForwards;
     drawAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
     [_arc addAnimation:drawAnimation forKey:@"drawCircleAnimation"];
+	_progress = progress;
 }
-
-
-
-
-
 
 -(void)stop{
 	[self.layer removeAllAnimations];
-	[_timer invalidate];
 }
 
 
