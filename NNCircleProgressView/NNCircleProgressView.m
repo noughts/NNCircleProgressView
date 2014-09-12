e5871c2034858a0ff5e26ce5d1f245b2e6799933
koichi yamamoto <koichi@dividual.jp>
2014-09-12 18:39:03 +0900

    a

--- NNCircleProgressView/NNCircleProgressView.m
+++ NNCircleProgressView/NNCircleProgressView.m
@@ -13,6 +13,8 @@
 	CGFloat _progress;
 	UIColor* _color;
 	CGFloat _intercept;// 円弧表示の切片
+	NSTimer* _timer;
+	BOOL _beat;
 }
 
 -(void)awakeFromNib{
@@ -25,7 +27,7 @@
 	
 	CGFloat rad1 = 0 * M_PI / 180;
 	CGFloat rad2 = 360 * M_PI / 180;
-    _arc.path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(0, 0) radius:radius startAngle:rad1 endAngle:rad2 clockwise:YES].CGPath;
+    _arc.path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(0, 0) radius:radius startAngle:rad1 endAngle:rad2 clockwise:NO].CGPath;
 	_arc.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
 	
     _arc.fillColor = [UIColor clearColor].CGColor;
@@ -50,6 +52,18 @@
     rotationAnimation.cumulative = YES;
     rotationAnimation.repeatCount = HUGE_VALF;
     [self.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
+	
+	_timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(time:) userInfo:nil repeats:YES];
+}
+
+
+-(void)time:(NSTimer*)timer{
+	if( _beat ){
+		[self setArcSizeFrom:0.1 to:0.5];
+	} else {
+		[self setArcSizeFrom:0.5 to:0.1];
+	}
+	_beat = !_beat;
 }
 
 
@@ -67,6 +81,22 @@
     [_arc addAnimation:drawAnimation forKey:@"drawCircleAnimation"];
 	_progress = progress;
 }
+
+-(void)setArcSizeFrom:(CGFloat)from to:(CGFloat)to{
+	CABasicAnimation *drawAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    drawAnimation.duration = 0.6;
+    drawAnimation.fromValue = @(from);
+    drawAnimation.toValue   = @(to);
+	drawAnimation.removedOnCompletion = NO;
+	drawAnimation.fillMode = kCAFillModeForwards;
+    drawAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    [_arc addAnimation:drawAnimation forKey:@"drawCircleAnimation"];
+}
+
+
+
+
+
 
 -(void)stop{
 	[self.layer removeAllAnimations];
